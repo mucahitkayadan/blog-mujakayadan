c4a3e7da9d2fdbd8fff5a00f45bab478b1b84767
mucahitkayadan <mujakayadan@outlook.com>
2025-01-01 01:40:17 -0600

    navlink fixed

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -4,11 +4,23 @@ export const navLinks = [
     path: "https://mujakayadan.com/#about",
   },
   {
+    title: "Education",
+    path: "https://mujakayadan.com/#education",
+  },
+  {
     title: "Work",
     path: "https://mujakayadan.com/#work",
   },
   {
+    title: "Skills",
+    path: "https://mujakayadan.com/#skills",
+  },
+  {
+    title: "Projects",
+    path: "https://mujakayadan.com/#projects",
+  },
+  {
     title: "Contact",
     path: "https://mujakayadan.com/#contact",
-  },
+  }
 ]; 
